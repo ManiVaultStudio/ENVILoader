ManiVaultStudio/ENVILoader

commit b8c79380700f56f5e802809d03a33f9f4497c7c8

Adhere to new core API [skip ci]

src/ENVILoader.cpp
@@ -53,14 +53,14 @@ bool ENVILoader::loadHeaderFromFile(std::string file)
 			std::string key = "";
 			std::string value = "";
 
-			int separatorIdx = line.find("=");
+			size_t separatorIdx = line.find("=");
 
 			if (separatorIdx >= 0)
 			{
 				key = trimString(line.substr(0, separatorIdx), { ' ', '\t', '\n' });
 
-				int objectOpenerIdx = line.find("{");
-				int objectCloserIdx = line.find("}");
+				size_t objectOpenerIdx = line.find("{");
+				size_t objectCloserIdx = line.find("}");
 
 				if (objectOpenerIdx < 0 || objectCloserIdx >= 0)
 				{
@@ -73,7 +73,7 @@ bool ENVILoader::loadHeaderFromFile(std::string file)
 					{
 						value += line;
 
-						int found = line.find("}");
+						size_t found = line.find("}");
 						if (found >= 0)
 						{
 							break;
@@ -312,19 +312,19 @@ bool ENVILoader::loadRaw(float ratio, int filter, bool flip)
 
 		points->setData(std::move(data), _header.imageBands);
 		points->setDimensionNames(_header.wavelengths);
-		events().notifyDatasetChanged(points);
+		events().notifyDatasetDataChanged(points);
 
 		auto images = _core->addDataset<Images>("Images", "images", Dataset<DatasetImpl>(*points));
 		events().notifyDatasetAdded(images);
 
-		images->setGuiName("Images");
+		images->setText("Images");
 		images->setType(ImageData::Type::Stack);
 		images->setNumberOfImages(1);
 		images->setImageSize(QSize(static_cast<int>(targetWidth), static_cast<int>(targetHeight)));
 		images->setNumberOfComponentsPerPixel(static_cast<unsigned int>(_header.imageBands));
 		images->setImageFilePaths(QStringList(QString::fromStdString(_header.rawFileName)));
 
-		events().notifyDatasetChanged(images);
+		events().notifyDatasetDataChanged(images);
 
 		return true;
 	}

src/SubsamplingRatioAction.cpp
@@ -17,8 +17,8 @@ const QMap<SubsamplingRatioAction::Ratio, float> SubsamplingRatioAction::default
 });
 
 SubsamplingRatioAction::SubsamplingRatioAction(QObject* parent) :
-    WidgetAction(parent),
-    _ratioAction(this, "Ratio", 1.0f, 100.0f, 50.0f, 50.0f, 1),
+    WidgetAction(parent, "SubsamplingRatioAction"),
+    _ratioAction(this, "Ratio", 1.0f, 100.0f, 50.0f, 50.0f),
     _defaultRatiosAction(this, "Default ratios", triggers.values().toVector())
 {
     setText("Ratio");

commit 0c994f79d84122b1e70c89fb9cbe00be07b018cf

Fixes an issue introduced in b8c79380700f56f5e802809d03a33f9f4497c7c8
types were changed to unsigned without adjusting the comparisons

src/ENVILoader.cpp
@@ -56,14 +56,14 @@ bool ENVILoader::loadHeaderFromFile(std::string file)
 
 			size_t separatorIdx = line.find("=");
 
-			if (separatorIdx >= 0)
+			if (separatorIdx != std::string::npos)
 			{
 				key = trimString(line.substr(0, separatorIdx), { ' ', '\t', '\n' , '\r' });
 
 				size_t objectOpenerIdx = line.find("{");
 				size_t objectCloserIdx = line.find("}");
 
-				if (objectOpenerIdx < 0 || objectCloserIdx >= 0)
+				if (objectOpenerIdx == std::string::npos || objectCloserIdx != std::string::npos)
 				{
 					value = trimString(line.substr(separatorIdx + 1, line.size() - 1), { ' ', '\t', '\n' , '\r' });
 				}
@@ -75,7 +75,7 @@ bool ENVILoader::loadHeaderFromFile(std::string file)
 						value += line;
 
 						size_t found = line.find("}");
-						if (found >= 0)
+						if (found != std::string::npos)
 						{
 							break;
 						}